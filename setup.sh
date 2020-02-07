0949ef728eedc47d7dd602fa3492c7f324bf3541
Yehia Mazen <yahya.m.mazen@gmail.com>
2020-02-07 16:47:12 +0200

    commit # 2

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-DATABASE_URL=postgres://kfinhdhssrlwqb:c87ab2ae0dd09cca840dda63d898cb24923fcbe557ccfd80a004dcd7852d681a@ec2-34-192-30-15.compute-1.amazonaws.com:5432/d3ciaoqetbckg5
+DATABASE_URL=postgres://gvfqcmnyvybbuk:4f753f507fa24736846c136fb5ca7c7b897f23462bffcdd9d0a0425034358222@ec2-52-202-185-87.compute-1.amazonaws.com:5432/d1vj4ufa22k8h8
