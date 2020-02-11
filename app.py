b53626966a59542fda57d6fb21b3fdd795ddd58a
Yehia Mazen <yahya.m.mazen@gmail.com>
2020-02-11 10:54:22 +0200

    commit #13

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exc
 from flask_cors import CORS
 
-from models import setup_db
+from models import setup_db, Trainer, Client, Session
 from auth import AuthError, requires_auth
 
 
@@ -16,6 +16,7 @@ def create_app(test_config=None):
 
     # db_drop_and_create_all()
 
+
     @app.route('/')
     def index():
         return jsonify({
@@ -23,7 +24,38 @@ def create_app(test_config=None):
             'message': 'Welcome the home page!'
         })
 
+    @app.route('/trainers', methods=['GET'])
+    def get_trainers():
+        trainers = Trainer.query.all()
+
+        trainers_formatted = []
+
+        for trainer in trainers:
+            trainers_formatted.append(trainer.format())
+        
+        return jsonify({
+            'success': True,
+            'trainers': trainers_formatted
+        })
+    
+    @app.route('/trainers', methods=['POST'])
+    def add_trainer():
+        request_data = request.get_json()
+
+        new_trainer = Trainer()
+        new_trainer.name = request_data['name']
+        new_trainer.gender = request_data['gender']
+        new_trainer.age = request_data['age']
+
+        new_trainer.insert()
+
+        return jsonify({
+            'success': True,
+            'new_trainer': new_trainer.format()
+        })
+
     return app
+
 
 
 app = create_app()
